d0523dda21975b63c7b7af2c1e2f0ae1dffa2c7e
adarshkushwaha <adarshkushwaha296@gmail.com>
2022-03-04 11:24:42 +0530

    day32

---
+++ String/ShuffleString.java
@@ -0,0 +1,20 @@
+package String;
+
+public class ShuffleString {
+    public static void main(String[] args) {
+        
+    }
+
+    static String restoreString(String s, int[] indices) {
+        int n = s.length();
+        StringBuilder ans = new StringBuilder(s);
+        
+        for(int i=0;i<n;i++){
+            ans.setCharAt(indices[i],s.charAt(i));
+        }
+        return ans.toString();
+        
+        
+        
+    }
+}

0ade244b1982597f66f802744a1106f6bf071b7f
adarshkushwaha <adarshkushwaha296@gmail.com>
2022-03-15 21:45:43 +0530

    day \45

---
+++ Maths/PrimeNumber.java
@@ -0,0 +1,24 @@
+package Maths;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        System.out.println(isPrime(0));
+    }
+
+    static boolean isPrime(int n){
+        if(n<=1){
+            return false;
+        }
+
+        int c = 2;
+
+        while(c*c<=n){
+            if(n%c==0){
+                return false;
+            }
+            c++; 
+        }
+
+        return true;
+    }
+}

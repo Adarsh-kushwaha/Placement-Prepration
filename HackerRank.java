9e04a968a300eb4fa18ca2d4f8d8b7549ff424f7
adarshkushwaha <adarshkushwaha296@gmail.com>
2022-05-01 23:52:07 +0530

    hackerRank test 30/04

---
+++ HackerRank.java
@@ -0,0 +1,67 @@
+import java.time.format.SignStyle;
+
+public class HackerRank {
+    public static void main(String[] args) {
+        String ans = getPhoneNumber("five one zero six triple eight nine six four");
+        System.out.println(ans);
+    }
+
+    public static String getPhoneNumber(String s) {
+        String[] text = s.split(" ");
+        String phoneNum = "";
+
+        for (int i = 0; i < text.length; i++) {
+
+            if (text[i].equals("double")) {
+                String singleNum = checkNum(text[i + 1]);
+                phoneNum = phoneNum + singleNum;
+            } else if (text[i].equals("tripple")) {
+                String singleNum = checkNum(text[i + 1]);
+                phoneNum = phoneNum + singleNum + singleNum;
+            } else {
+                String singleNum = checkNum(text[i]);
+                phoneNum = phoneNum + singleNum;
+
+            }
+
+        }
+
+        return phoneNum;
+    }
+
+    public static String checkNum(String numStr) {
+        if (numStr.equals("one")) {
+            return "1";
+        }
+
+        if (numStr.equals("two")) {
+            return "2";
+        }
+        if (numStr.equals("three")) {
+            return "3";
+        }
+        if (numStr.equals("four")) {
+            return "4";
+        }
+        if (numStr.equals("five")) {
+            return "5";
+        }
+        if (numStr.equals("six")) {
+            return "6";
+        }
+        if (numStr.equals("seven")) {
+            return "7";
+        }
+        if (numStr.equals("eight")) {
+            return "8";
+        }
+        if (numStr.equals("nine")) {
+            return "9";
+        }
+        if (numStr.equals("zero")) {
+            return "0";
+        }
+
+        return "";
+    }
+}

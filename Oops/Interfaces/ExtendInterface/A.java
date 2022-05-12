cea7eae2021c911bc1a4903d967acf1f919682cd
adarshkushwaha <adarshkushwaha296@gmail.com>
2022-05-12 12:15:19 +0530

    ques

--- Oops/Interfaces/ExtendInterface/A.java
+++ Oops/Interfaces/ExtendInterface/A.java
@@ -4,7 +4,7 @@ public interface A {
 
     //static method requires body and should be define inside interface;
     //they can be called using the name of interface
-    static void printSomething(){
+     static void printSomething(){
         System.out.println("Hello from another side");
     }
 
--- test.java
+++ test.java
@@ -1,24 +1,33 @@
-// import java.text.SimpleDateFormat;
-// import java.util.Arrays;
-// import java.util.Date;
+import java.util.*;  
+import java.util.ArrayList ;
+public class Test{
 
-public class test {
-  public static void main(String[] args) {
-    
-  }
+     public static  List<Character>solve(List<Character>list){
+          ArrayList<Character> al = new ArrayList<Character>();
+           Set<Character> s = new HashSet<Character>();
 
-  static int[] reverse(int arr[]) {
-    int start = arr.length / 2;
-    int end = arr.length - 1;
+        for(Character name : list) {
+            if(s.add(name) == false)
+                System.out.println(name + "is duplicated");
+                al.add(name);
+        }
+         return al;
+     } 
+      
+      public static void main(String[] args)
+     {  
+         
+   ArrayList<Character> list= new ArrayList<Character>();
+        list.add('a');
+        list.add('b');
+        list.add('b');
+        list.add('c');
+        list.add('c');
+        solve(list);
+        for (int i = 0; i < list.size(); i++)
+        System.out.print(list.get(i) + " ");
+        
 
-    while (start < end) {
-      int temp = arr[start];
-      arr[start] = arr[end];
-      arr[end] = temp;
-      start++;
-      end--;
+       
     }
-
-    return arr;
-  }
 }

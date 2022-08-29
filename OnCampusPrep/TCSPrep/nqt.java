d16f3f5db07c9774ee31f436a2770e32513dc38c
adarshkushwaha <adarshkushwaha296@gmail.com>
2022-08-29 11:26:07 +0530

    new sol

--- OnCampusPrep/TCSPrep/nqt.java
+++ OnCampusPrep/TCSPrep/nqt.java
@@ -1,31 +1,73 @@
-package OnCampusPrep.TCSPrep;
 
-import java.util.Arrays;
+class NQT {
 
-public class nqt {
-    public static void main(String[] args) {
+	// Function to find the reduced string
+	public static String reduced_String(int k, String s)
+	{
+		// Base Case
+		if (k == 1) {
+			String ans = "";
+			return ans;
+		}
 
-    }
+		// Creating a stack of type Pair
+		Stack<Pair> st = new Stack<Pair>();
 
-    static void removeDuplicate(String string, int r) {
-        int index = 0;
-        char[] str = string.toCharArray();
-        int length = str.length;
-       
-        for (int i = 0; i < length; i++) {
+		int l = s.length();
+		int ctr = 0;
 
-         
-            int j;
-            for (j = 0; j < i; j++) {
-                if (str[i] == str[j]) {
-                    break;
-                }
-            }
+		
+		for (int i = 0; i < l; i++) {
 
-            if (j == i) {
-                str[index++] = str[i];
-            }
-        }
-        System.out.println(String.valueOf(Arrays.copyOf(str, index)));
-    }
+			
+			if (st.size() == 0) {
+				st.push(new Pair(s.charAt(i), 1));
+				continue;
+			}
+
+			if (st.peek().c == s.charAt(i)) {
+				Pair p = st.peek();
+				st.pop();
+				p.ctr += 1;
+				if (p.ctr == k) {
+					continue;
+				}
+				else {
+					st.push(p);
+				}
+			}
+			else {
+
+				st.push(new Pair(s.charAt(i), 1));
+			}
+		}
+
+		String ans = "";
+		while (st.size() > 0) {
+			char c = st.peek().c;
+			int cnt = st.peek().ctr;
+			while (cnt-- > 0)
+				ans = c + ans;
+			st.pop();
+		}
+		return ans;
+	}
+
+	// public static void main(String[] args)
+	// {
+	// 	int k = 2;
+	// 	String st = "iinpuuut";
+	// 	String ans = reduced_String(k, st);
+	// 	System.out.println(ans);
+	// }
+
+	// static class Pair {
+	// 	char c;
+	// 	int ctr;
+	// 	Pair(char c, int ctr)
+	// 	{
+	// 		this.c = c;
+	// 		this.ctr = ctr;
+	// 	}
+	// }
 }

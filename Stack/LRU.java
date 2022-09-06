866ed3e348513fbfa14bc3ddd386e191376060a1
adarshkushwaha <adarshkushwaha296@gmail.com>
2022-09-06 09:23:32 +0530

    stack question

---
+++ Stack/LRU.java
@@ -0,0 +1,67 @@
+package Stack;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRU {
+    class Node {
+        Node prev;
+        Node next;
+        int key;
+        int value;
+
+        Node(int _key, int _value) {
+            key = _key;
+            value = _value;
+        }
+    }
+
+    Node head = new Node(0, 0);
+    Node tail = new Node(0, 0);
+    Map<Integer, Node> map = new HashMap<>();
+    int capacity;
+
+    public LRU(int _capacity) {
+        capacity = _capacity;
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    // remove function
+    private void remove(Node node) {
+        map.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    // insert Functionality
+    private void insert(Node node) {
+        map.put(node.key, node);
+        node.next = head.next;
+        head.next = node;
+        node.next.prev = node;
+        node.prev = head;
+    }
+
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            remove(node);
+            insert(node);
+            return node.value;
+        } else {
+            return -1;
+        }
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            remove(map.get(key));
+        }
+
+        if (map.size() == capacity) {
+            remove(tail.prev);
+        }
+        insert(new Node(key, value));
+    }
+}
